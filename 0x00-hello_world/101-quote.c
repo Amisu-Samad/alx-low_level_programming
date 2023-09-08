c04114415c6678320f826574b58eec7271663ad2
Amisu-Samad <adeboladamilare70@gmail.com>
2023-09-07 20:18:06 -0700

    Print without prinf and puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
- * main - a programe that print a line to the standard error
+ * main - printing without printf and puts
  *
- * Return: always (1)
+ * Return: always (0)
  *
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
-	return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (0);
 }
