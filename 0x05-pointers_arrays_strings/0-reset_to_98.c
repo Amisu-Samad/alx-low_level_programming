de724fdb1282757d8773e1f6655969d7aa70be76
Amisu-Samad <adeboladamilare70@gmail.com>
2023-09-18 19:14:49 -0700

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * Funtion name - reset_to_98
+ *@n: function parameter
+ * Return: void
+ */
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * Function name - swap_int
+ * @a: first parameter
+ * @b: second parameter
+ */
+void swap_int(int *a, int *b);
+{
+int c;
+c = *a;
+*a = *b;
+*b = *c;
+}
