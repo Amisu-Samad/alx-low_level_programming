d63bc1c038c0c21e0995a01b88d349ee21804f8a
Amisu-Samad <adeboladamilare70@gmail.com>
2023-10-17 04:58:26 -0700

    Preprocessor

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file
+ * ain - a program that prints compiled file name
+ * Return: Always 0 
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
